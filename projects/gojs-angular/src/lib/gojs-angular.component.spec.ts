518db82a65eb6dfcc08b162da19899365fe50323
Simon Sarris <simonsarris@users.noreply.github.com>
2026-01-08 10:41:05 -0500

    fix test

--- projects/gojs-angular/src/lib/gojs-angular.component.spec.ts
+++ projects/gojs-angular/src/lib/gojs-angular.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { GojsAngularComponent } from './gojs-angular.component';
+import go from 'gojs';
+import { DiagramComponent } from './diagram.component';
 
 describe('GojsAngularComponent', () => {
-  let component: GojsAngularComponent;
-  let fixture: ComponentFixture<GojsAngularComponent>;
+  let component: DiagramComponent;
+  let fixture: ComponentFixture<DiagramComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GojsAngularComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(GojsAngularComponent);
+      imports: [DiagramComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DiagramComponent);
     component = fixture.componentInstance;
+    component.initDiagram = () => new go.Diagram();
     fixture.detectChanges();
   });
 
